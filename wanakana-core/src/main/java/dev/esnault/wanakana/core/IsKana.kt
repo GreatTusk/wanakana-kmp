34282b291324888a02f81d39cb3b3cf15f19ba6f
Fernando <131561675+GreatTusk@users.noreply.github.com>
2025-02-05 22:22:01 -0300

    first commit

--- wanakana-core/src/main/java/dev/esnault/wanakana/core/IsKana.kt
+++
@@ -1,8 +0,0 @@
-package dev.esnault.wanakana.core
-
-import dev.esnault.wanakana.core.extension.isKana
-
-
-internal fun isKana(input: String): Boolean = input.all(Char::isKana)
-
-internal fun isKana(input: Char): Boolean = isHiragana(input) || isKatakana(input)
